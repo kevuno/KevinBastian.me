12889b83122ca40e5bfc62396a6ac8ff72d279d1
kjb5642 <kjb5642@rit.edu>
2016-07-15 02:42:30 -0400

    Added info and changed collors for the planifier

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,10 @@
 console.log("12312355555");
+slow = 1000;
+medium = 500;
+fast = 200;
+
 $("#name").hover(function(){
-		$("#jusef").toggle(1000);
-		$("#perez").toggle(1000);
+		$("#jusef").toggle(fast);
+		$("#perez").toggle(fast);
 });
 $("#college").load("college.html");
